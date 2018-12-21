c4ed19b0b449ece9f7ebadad2cdeec21766e9cac
PriyaSingh31 <35927218+PriyaSingh31@users.noreply.github.com>
2018-12-21 12:45:19 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,16 @@
-#print("enter any number")
-#a=int(input())
-#a=a+3
-#print("value of a is",+a)
-#if a<10:
-#    print("you're correct")
-#else:
-#    print("value is not greater than 10")
-
+#takes input from user
 print("enter two digit number")
 a=int(input())
 print("enter one more two digit number")
 b=int(input())
-unitd=int(b%10)
-prod1=a*unitd
-tensd=int(b/10)
-prod2=*(tensd*10)
+#finding the unit digit of second number
+unitdigit=int(b%10)
+#multiply unit digit number by first number
+prod1=a*unitdigit
+#finding tens digit of second number
+tensdigit=int(b/10)
+#multiply tens digit by first number
+prod2=(tensdigit*10)
+#the result will be the sum of these products
 result=prod1+prod2
 print("the product of two numbers is {0}".format(result))
